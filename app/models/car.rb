db5e025077a64651155bfa0a25fdbb6e5aeb679b
96StanleyLee <96stanleylee@gmail.com>
2020-02-24 13:46:16 -0500

    Done.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,43 @@
 class Car
-
-  attr_reader :make, :model
-
-  def initialize(make, model, classification)
-    @make = make
-    @model = model
-    @classification = classification
-  end
-
-end
+  attr_reader :car_name, :car_model, :car_classification, :owner, :mechanic
+  
+  @@all = []
+  
+  def initialize(car_brand, car_model,car_classification, owner, mechanic)
+      @car_brand = car_brand 
+      @car_model = car_model
+      @car_classification = car_classification 
+      @owner = owner 
+      @mechanic = mechanic
+      @@all.push(self)
+  end 
+  
+  
+  
+  def self.all
+      @@all 
+  end 
+  
+  def self.car_classifications
+      self.all.map do |cars| 
+          cars.car_classification 
+      end 
+  end 
+  
+  
+  def self.match_car_classification(classification)
+    mechanics =  self.all.map do |cars|
+        if cars.mechanic.specialization == classification
+          cars.mechanic
+       end
+      end 
+      mechanics.compact
+  end 
+  
+  
+  
+  
+  
+  
+  
+  end 
